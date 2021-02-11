0b1037322a814516c17c84563803b85a5f62562c
NTX123-456 <73065899+NTX123-456@users.noreply.github.com>
2021-02-11 22:48:39 +0800

    Added 2 new pages
    2 new pages, rewards page and user input information page

---
+++ Rewards.js
@@ -0,0 +1,12 @@
+function click3stprize(){
+    alert("Congratulations !You have claimed the third prize");
+    window.location.assign('UserInfo.html');
+}
+function click2stprize(){
+    alert("Congratulations! You have claimed the second prize");
+    window.location.assign('UserInfo.html');
+}
+function clickgrandprize(){
+    alert("Congratulations! You have claimed the grand prize");
+     window.location.assign('UserInfo.html');
+}
